05c7ffabb785b7fa667660e36e036971ef9d4dfe
Sébastien Temprado <stemprado@gmail.com>
2017-02-06 22:32:52 +0100

    Step 14 ok

--- app/app.module.js
+++ app/app.module.js
@@ -1,3 +1,3 @@
 // Define the `phonecatApp` module
-var phonecatApp = angular.module('phonecatApp', ['ngRoute','phoneListModule', 'greetUserModule', 'phoneDetail', 'core']);
+var phonecatApp = angular.module('phonecatApp', ['ngRoute','phoneListModule', 'greetUserModule', 'phoneDetail', 'core', 'ngAnimate']);
 
